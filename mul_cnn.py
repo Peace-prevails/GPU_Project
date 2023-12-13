76dbc1bbe5ae7913ba93b31fefd7ae5509224ece
Peace-prevails <107624003+Peace-prevails@users.noreply.github.com>
2023-12-13 12:30:02 -0500

    Update mul_cnn.py

--- mul_cnn.py
+++ mul_cnn.py
@@ -7,24 +7,24 @@ from torch.utils.data import DataLoader, DistributedSampler
 import torch.nn.functional as F
 import torch.distributed as dist
 import time
-# 定义您的模型（ComplexCNN）...
+
 class ComplexCNN(nn.Module):
     def __init__(self):
         super(ComplexCNN, self).__init__()
-        # 第一层卷积
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)  # 输入通道为1，输出通道为32
+
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)  
         self.bn1 = nn.BatchNorm2d(32)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)  # 池化层
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2) 
 
-        # 第二层卷积
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)  # 输入通道为32，输出通道为64
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1) 
         self.bn2 = nn.BatchNorm2d(64)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)  # 池化层
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) 
 
-        # 全连接层
-        self.fc1 = nn.Linear(64 * 7 * 7, 1024)  # 注意计算全连接层的输入维度
+   
+        self.fc1 = nn.Linear(64 * 7 * 7, 1024)  
         self.dropout1 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(1024, 10)  # 输出层，10个类别
+        self.fc2 = nn.Linear(1024, 10)  
 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
@@ -32,10 +32,10 @@ class ComplexCNN(nn.Module):
         x = F.relu(self.bn2(self.conv2(x)))
         x = self.pool2(x)
 
-        x = x.view(-1, 64 * 7 * 7)  # 展平层
+        x = x.view(-1, 64 * 7 * 7) 
         x = F.relu(self.fc1(x))
         x = self.dropout1(x)
-        x = self.fc2(x)  # 未使用激活函数，因为后面会使用交叉熵损失函数
+        x = self.fc2(x) 
         return x
 
 def setup(rank, world_size):
@@ -51,13 +51,13 @@ def cleanup():
 def train(rank, world_size, epochs=10):
     setup(rank, world_size)
 
-    # 准备数据集
+
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
     train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
     train_sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank)
     train_loader = DataLoader(train_dataset, batch_size=256, shuffle=False, sampler=train_sampler)
 
-    # 初始化网络和优化器
+
     device = torch.device("cuda", rank)
     model = ComplexCNN().to(device)
     model = nn.parallel.DistributedDataParallel(model, device_ids=[rank])
@@ -65,8 +65,8 @@ def train(rank, world_size, epochs=10):
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
-    # 训练过程
-    start_time = time.time()  # 开始计时
+
+    start_time = time.time()  
     model.train()
     for epoch in range(epochs):
         train_sampler.set_epoch(epoch)
@@ -77,7 +77,7 @@ def train(rank, world_size, epochs=10):
             loss = criterion(output, target)
             loss.backward()
             optimizer.step()
-    end_time = time.time()  # 结束计时
+    end_time = time.time()  
     total_time = end_time - start_time
     print(f"Total training time ddp: {total_time:.2f} seconds")
     cleanup()
